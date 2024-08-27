c659a71b89b05fde5b3629f55aa60706aa314dd9
PriyeshRai369 <priyeshrai369@gmail.com>
2024-08-27 17:16:05 +0530

    Complete the profile page

--- src/components/RegistrationForm/RegisterSuccess.jsx
+++ src/components/RegistrationForm/RegisterSuccess.jsx
@@ -7,9 +7,17 @@ export default function RegisterSuccess() {
         <div className="title">
           <h1>Process the payment</h1>
         </div>
-        <p style={{color:"var(--text-color)",fontSize:"18px",fontWeight:"500"}}>Please process the payment of{" "}
-        <span style={{ color: "red", fontWeight: "500" }}>Rs. 3000</span> for
-        Lifetime Membership of SJSAA.</p>
+        <p
+          style={{
+            color: "var(--text-color)",
+            fontSize: "18px",
+            fontWeight: "500",
+          }}
+        >
+          After registration, please process the payment of{" "}
+          <span style={{ color: "red", fontWeight: "500" }}>Rs. 3000</span> for
+          Lifetime Membership of SJSAA.
+        </p>
         <div className="row row-gap-4">
           <div className="col-lg-6">
             <div className="paymentContainer">
@@ -17,15 +25,30 @@ export default function RegisterSuccess() {
                 <h1>Bank Details</h1>
               </div>
               <div className="bankDetails">
-              <ul>
-                  <li>Account name :- <span>ST.JOHNS SCHOOL (DLW) ALUMNI ASSOCIATION</span></li>
-                  <li>Account number :- <span>30537291315</span></li>
-                  <li>IFSC Code :- <span>SBIN0002552</span></li>
-                  <li>Bank :- <span>STATE bank of india, industrial estate, VARANASI</span></li>
+                <ul>
+                  <li>
+                    Account name :-{" "}
+                    <span>ST.JOHNS SCHOOL (DLW) ALUMNI ASSOCIATION</span>
+                  </li>
+                  <li>
+                    Account number :- <span>30537291315</span>
+                  </li>
+                  <li>
+                    IFSC Code :- <span>SBIN0002552</span>
+                  </li>
+                  <li>
+                    Bank :-{" "}
+                    <span>
+                      STATE bank of india, industrial estate, VARANASI
+                    </span>
+                  </li>
                 </ul>
               </div>
-
-              <h5>After making the payment, send the receipt/copy/pic of payment to our E-Mail ID : sjsaadlwvns@gmail.com</h5>
+              <hr style={{ color: "white", height: "3px" }} />
+              <h5>
+                After making the payment, send the receipt/copy/pic of payment
+                to our E-Mail ID : sjsaadlwvns@gmail.com
+              </h5>
             </div>
           </div>
           <div className="col-lg-6">
