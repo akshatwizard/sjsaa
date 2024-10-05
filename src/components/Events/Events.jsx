2fbd5a2c72cb38c12dd8fc93cc64084a6a6b5c5f
PriyeshRai369 <priyeshrai369@gmail.com>
2024-10-05 12:32:40 +0530

    Update event page.

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Fancybox from "../ImageZoom/Fancybox";
 import { Link, useLocation } from "react-router-dom";
 import { scrollToTop } from "../../helper/scroll";
+import axios from "axios";
 
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
@@ -12,6 +13,25 @@ gsap.registerPlugin(ScrollTrigger);
 export default function Events() {
   const container = useRef();
   const location = useLocation();
+  const [allEvent, setAllEvent] = useState();
+
+  useEffect(() => {
+    const getAllEvent = async () => {
+      const formData = new FormData();
+      formData.append("event_list", "event_list");
+
+      try {
+        const response = await axios.post(
+          "https://www.gdsons.co.in/draft/sjs/event-listapi",
+          formData
+        );
+        setAllEvent(response?.data);
+      } catch (error) {
+        console.error("Error fetching gallery images:", error);
+      }
+    };
+    getAllEvent();
+  }, []);
 
   // Access the current path
   const currentPath = location.pathname;
@@ -53,179 +73,70 @@ export default function Events() {
         <div className="title eveTitle">
           <h1>Events</h1>
         </div>
+
         <div className="row mb-5 animate">
-          <div className="col-lg-12">
-            <div className="eventContainer">
-              <div className="row row-gap-3">
-                <div className="col-lg-2 col-md-2 col-sm-3 text-center py-2">
-                  <div className="dateContainer">
-                    <p>July</p>
-                    <h2>14</h2>
-                  </div>
-                </div>
-                <div className="col-lg-10 col-md-10 col-sm-9">
-                  <div className="row">
-                    <div className="col-lg-9 col-md-9">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          <div className="heading">
-                            <a href="/">
-                              <h1>
-                                AGM Elections held @ Hotel Royal Livin, Gurudham
-                                Colony, Varanasi
-                                <i className="fa-solid fa-arrow-right "></i>
-                              </h1>
-                            </a>
-                          </div>
+          {allEvent?.map((event) => {
+            const eventDate = new Date(event.event_date);
+            const month = eventDate.toLocaleString("default", {
+              month: "long",
+            });
+            const day = eventDate.getDate();
+
+            return (
+              <div className="col-lg-12" key={event.event_nid}>
+                <div className="add-new-event-container">
+                  <div className="eventContainer">
+                    <div className="row row-gap-3">
+                      <div className="col-lg-2 col-md-2 col-sm-3 text-center py-2">
+                        <div className="dateContainer">
+                          <p>{month}</p>
+                          <h2>{day}</h2>
                         </div>
-                        <div className="col-lg-12 my-2">
-                          <div className="eventDescription">
-                            <p>
-                              Rajkumar Agarwal Chairman of Alumni Association
-                              Varanasi. The annual general meeting of the Alumni
-                              Association of St. John's School Bareka was held
-                              on Sunday. Rajkumar Agarwal was elected chairman,
-                              Puneet Agarwal vice chairman, Abhinav Pandey
-                              president, Digvijay Singh vice president, Harsh
-                              Madhok secretary and Manish Kataria treasurer.
-                            </p>
+                      </div>
+                      <div className="col-lg-10 col-md-10 col-sm-9">
+                        <div className="row">
+                          <div className="col-lg-9 col-md-9">
+                            <div className="row">
+                              <div className="col-lg-12">
+                                <div className="heading">
+                                  <a href="#">
+                                    <h1>
+                                      {event.event_title}
+                                      <i className="fa-solid fa-arrow-right "></i>
+                                    </h1>
+                                  </a>
+                                </div>
+                              </div>
+                              <div className="col-lg-12 my-2">
+                                <div className="eventDescription">
+                                  <p>{event.event_details}</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-lg-3 col-md-3">
+                            <Fancybox>
+                              <div
+                                className="eventImagesContainer"
+                                data-fancybox="gallery"
+                                href={event.event_images}
+                              >
+                                <img
+                                  src={event.event_images}
+                                  alt={event.event_title}
+                                  loading="lazy"
+                                />
+                              </div>
+                            </Fancybox>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <div className="col-lg-3 col-md-3">
-                      <Fancybox>
-                        <div
-                          className="eventImagesContainer"
-                          data-fancybox="gallery"
-                          href="/images/events/agm-ellection.jpeg"
-                        >
-                          <img
-                            src="/images/events/agm-ellection.jpeg"
-                            alt=""
-                            loading="lazy"
-                          />
-                        </div>
-                      </Fancybox>
-                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mb-5 animate">
-          <div className="col-lg-12">
-            <div className="eventContainer">
-              <div className="row row-gap-3">
-                <div className="col-lg-2 col-md-2 col-sm-3 text-center py-2">
-                  <div className="dateContainer">
-                    <p>July</p>
-                    <h2>21</h2>
-                  </div>
-                </div>
-                <div className="col-lg-10 col-md-10 col-sm-9">
-                  <div className="row">
-                    <div className="col-lg-9 col-md-9">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          <div className="heading">
-                            <a href="/">
-                              <h1>
-                                Grand Opening of Quick Bites
-                                <i className="fa-solid fa-arrow-right "></i>
-                              </h1>
-                            </a>
-                          </div>
-                        </div>
-                        <div className="col-lg-12 my-2">
-                          <div className="eventDescription">
-                            <p>
-                              A grand opening of a new outlet Quick Bites by
-                              Harsh Jhunjhunwala, featuring all your favorites from Sri
-                              Annapurna Foods under one roof was attended by our fellow
-                              Alumni!
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-3 col-md-3">
-                      <Fancybox>
-                        <div
-                          className="eventImagesContainer"
-                          data-fancybox="gallery"
-                          href="/images/events/quick-bites.jpeg"
-                        >
-                          <img
-                            src="/images/events/quick-bites.jpeg"
-                            alt=""
-                            loading="lazy"
-                          />
-                        </div>
-                      </Fancybox>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mb-5 animate">
-          <div className="col-lg-12">
-            <div className="eventContainer">
-              <div className="row row-gap-3">
-                <div className="col-lg-2 col-md-2 col-sm-3 text-center py-2">
-                  <div className="dateContainer">
-                    <p>August</p>
-                    <h2>03</h2>
-                  </div>
-                </div>
-                <div className="col-lg-10 col-md-10 col-sm-9">
-                  <div className="row">
-                    <div className="col-lg-9 col-md-9">
-                      <div className="row">
-                        <div className="col-lg-12">
-                          <div className="heading">
-                            <a href="/">
-                              <h1>
-                                Movie Night :- Auron May Kahan Dum Tha
-                                <i className="fa-solid fa-arrow-right "></i>
-                              </h1>
-                            </a>
-                          </div>
-                        </div>
-                        <div className="col-lg-12 my-2">
-                          <div className="eventDescription">
-                            <p>
-                              A movie night show was hosted by Alumni Association 
-                              which was attended by fellow alumni members and was
-                              a grand success!
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-3 col-md-3">
-                      <Fancybox>
-                        <div
-                          className="eventImagesContainer"
-                          data-fancybox="gallery"
-                          href="/images/events/movie.jpg"
-                        >
-                          <img
-                            src="/images/events/movie.jpg"
-                            alt=""
-                            loading="lazy"
-                          />
-                        </div>
-                      </Fancybox>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
 
         {currentPath === "/event" ? (
