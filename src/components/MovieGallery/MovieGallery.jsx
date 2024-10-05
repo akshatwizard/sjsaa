c2dc3cb7c4d28b8809b0c0ce6043d1daa91e764a
PriyeshRai369 <priyeshrai369@gmail.com>
2024-10-05 12:21:57 +0530

    Update Gallery.

--- src/components/MovieGallery/MovieGallery.jsx
+++ src/components/MovieGallery/MovieGallery.jsx
@@ -1,9 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import { movieImages } from "../../helper/movieImageSrc";
 import Fancybox from "../ImageZoom/Fancybox";
+import axios from "axios";
 
 export default function MovieGallery() {
+  const [allimage, setAllImage] = useState();
 
+ useEffect(()=>{
+  const getAllImage = async () => {
+    const formData = new FormData();
+    formData.append("gallery_list", "gallery_list");
+
+    try {
+      const response = await axios.post(
+        "https://www.gdsons.co.in/draft/sjs/gallery-list",
+        formData
+      );
+      setAllImage(response?.data);
+    } catch (error) {
+      console.error("Error fetching gallery images:", error);
+    }
+  };
+  getAllImage();
+ },[])
+ 
   return (
     <section className="sectionContainer" >
       <div className="container">
@@ -13,7 +32,7 @@ export default function MovieGallery() {
         <div className="row">
           <div className="col-lg-12">
             <div className="galleryAllImageContainer">
-              {movieImages.map((image) => (
+              {allimage?.map((image) => (
                 <Fancybox
                   options={{
                     Carousel: {
@@ -23,11 +42,11 @@ export default function MovieGallery() {
                 >
                   <div
                     className="imageGrid"
-                    key={image.id}
+                    key={image.gallery_id}
                     data-fancybox="gallery"
-                    href={image.src}
+                    href={image.gallery_file}
                   >
-                    <img src={image.src} alt={image.alt} loading="lazy"/>
+                    <img src={image.gallery_file} loading="lazy"/>
                   </div>
                 </Fancybox>
               ))}
