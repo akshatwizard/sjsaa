75755471e98cd02af1c6a0e9c4b0e1ae2ba2a1ed
PriyeshRai369 <priyeshrai369@gmail.com>
2025-01-20 15:04:00 +0530

    update gallery.

--- src/components/MovieGallery/MovieGallery.jsx
+++ src/components/MovieGallery/MovieGallery.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import Fancybox from "../ImageZoom/Fancybox";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export default function MovieGallery() {
   const [allimage, setAllImage] = useState();
+  const { albumId } = useParams();
+  const [imgData, setImgData] = useState()
+  // const dt = Object.values(allimage)
+  // console.log(dt);
 
   useEffect(() => {
     const getAllImage = async () => {
       const formData = new FormData();
       formData.append("gallery_list", "gallery_list");
+      formData.append("albumnid", albumId)
 
       try {
         const response = await axios.post(
@@ -16,24 +22,25 @@ export default function MovieGallery() {
           formData
         );
         setAllImage(response?.data);
+        setImgData(Object.values(response?.data).filter(item => item.gallery_id))
       } catch (error) {
         console.error("Error fetching gallery images:", error);
       }
     };
     getAllImage();
   }, [])
-
+  // console.log(imgData);
   return (
     <section className="sectionContainer" >
       <div className="container">
         <div className="title">
-          <h1>Images From Auron Mein Kahan Dum Tha Movie.</h1>
+          <h1>{allimage?.gallery_title}</h1>
         </div>
         <div className="row">
           <div className="col-lg-12">
             <div className="galleryAllImageContainer">
               <div className="row">
-                {allimage?.map((image) => (
+                {imgData?.map((image) => (
                   <div className="col-lg-3 col-md-6 col-sm-6 col-12">
                     <Fancybox
                       options={{
