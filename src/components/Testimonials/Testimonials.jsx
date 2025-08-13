638851a9189310c55fed608ea97d275c76038861
priyesshrai <priyeshrai369@gmail.com>
2025-08-13 15:42:57 +0530

    update cards

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -37,112 +37,118 @@ export default function Testimonials() {
   }, []);
 
   return (
-    <section className="sectionContainer">
-      <div className="container">
-        <div className="title">
-          <h1>Testimonials</h1>
-        </div>
-        <div className="row row-gap-3">
-          <div className="col-lg-12">
-            <div className="row">
-              <Carousel
-                responsive={responsive}
-                autoPlay={true}
-                swipeable={true}
-                draggable={true}
-                showDots={true}
-                infinite={true}
-                partialVisible={false}
-                dotListClass="custom-dot-list-style"
-              >
+    <>
+      {
+        allTestimonials && (
+          <section className="sectionContainer">
+            <div className="container">
+              <div className="title">
+                <h1>Testimonials</h1>
+              </div>
+              <div className="row row-gap-3">
+                <div className="col-lg-12">
+                  <div className="row">
+                    <Carousel
+                      responsive={responsive}
+                      autoPlay={true}
+                      swipeable={true}
+                      draggable={true}
+                      showDots={true}
+                      infinite={true}
+                      partialVisible={false}
+                      dotListClass="custom-dot-list-style"
+                    >
 
-                {allTestimonials?.map((testimonial, idx) => {
-                  const trimText = (text, maxLength = 300) => {
-                    if (!text) return { text: '', isTrimmed: false };
-                    if (text?.length <= maxLength) return { text, isTrimmed: false };
-                    return {
-                      text: text.substring(0, maxLength).trim() + '...',
-                      isTrimmed: true
-                    };
-                  };
-                  const { text: trimmedText, isTrimmed } = trimText(testimonial?.testmsg, 200);
+                      {allTestimonials?.map((testimonial, idx) => {
+                        const trimText = (text, maxLength = 300) => {
+                          if (!text) return { text: '', isTrimmed: false };
+                          if (text?.length <= maxLength) return { text, isTrimmed: false };
+                          return {
+                            text: text.substring(0, maxLength).trim() + '...',
+                            isTrimmed: true
+                          };
+                        };
+                        const { text: trimmedText, isTrimmed } = trimText(testimonial?.testmsg, 200);
 
-                  return (
-                    <div key={idx}>
-                      <div className="testimonialsContainer">
-                        <div className="image-box">
-                          <Fancybox style={{ height: '200px' }}>
-                            <div
-                              style={{ height: '200px' }}
-                              data-fancybox="gallery"
-                              href={testimonial.testimg_large}
-                            >
-                              <img
-                                src={testimonial.testimg_small}
-                                className="userProfile"
-                                alt=""
-                              />
+                        return (
+                          <div key={idx}>
+                            <div className="testimonialsContainer">
+                              <div className="image-box">
+                                <Fancybox style={{ height: '200px' }}>
+                                  <div
+                                    style={{ height: '200px' }}
+                                    data-fancybox="gallery"
+                                    href={testimonial.testimg_large}
+                                  >
+                                    <img
+                                      src={testimonial.testimg_small}
+                                      className="userProfile"
+                                      alt=""
+                                    />
+                                  </div>
+                                </Fancybox>
+                                <img
+                                  src="https://uiparadox.co.uk/templates/teach-me/assets/media/icons/quotes.png"
+                                  className="qt"
+                                  alt=""
+                                />
+                              </div>
+                              <hr style={{ color: "#919191ff" }} />
+                              <div className="testimonialsDetails">
+                                <p>
+                                  {trimmedText}
+                                </p>
+                                {isTrimmed && (
+                                  <span className="testimonials-rdmor"
+                                    onClick={() => {
+                                      setOpenClickedTestimonial(true);
+                                      setClickedTestimonial(testimonial)
+                                    }}>
+                                    Read more
+                                  </span>
+                                )}
+                              </div>
                             </div>
-                          </Fancybox>
-                          <img
-                            src="https://uiparadox.co.uk/templates/teach-me/assets/media/icons/quotes.png"
-                            className="qt"
-                            alt=""
-                          />
-                        </div>
-                        <hr style={{ color: "#919191ff" }} />
-                        <div className="testimonialsDetails">
-                          <p>
-                            {trimmedText}
-                          </p>
-                          {isTrimmed && (
-                            <span className="testimonials-rdmor"
-                              onClick={() => {
-                                setOpenClickedTestimonial(true);
-                                setClickedTestimonial(testimonial)
-                              }}>
-                              Read more
-                            </span>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })}
+                          </div>
+                        );
+                      })}
 
-              </Carousel>
-            </div>
-          </div>
-        </div>
-      </div>
-      {clickedTestimonial !== null && openClickedTestimonial && (
-        <div className="full-testimonials-container">
-          <div className="full-testimonials-wraper">
-            <div className="full-testimonials-box">
-              <div
-                className="testimonials-box-close-btn"
-                onClick={() => {
-                  setOpenClickedTestimonial(false);
-                  setClickedTestimonial(null);
-                }}
-                style={{ cursor: 'pointer' }}
-              >
-                <i className="fa-solid fa-xmark"></i>
+                    </Carousel>
+                  </div>
+                </div>
               </div>
-              <div className="box-testimonials-image">
-                <img
-                  src={clickedTestimonial?.testimg_small}
-                  alt="Testimonial"
-                  className="modal-testimonial-image"
-                />
-              </div>
-              <p>
-                {clickedTestimonial?.testmsg || 'No testimonial text available'}
-              </p>
             </div>
-          </div>
-        </div>
-      )}
-    </section>
+            {clickedTestimonial !== null && openClickedTestimonial && (
+              <div className="full-testimonials-container">
+                <div className="full-testimonials-wraper">
+                  <div className="full-testimonials-box">
+                    <div
+                      className="testimonials-box-close-btn"
+                      onClick={() => {
+                        setOpenClickedTestimonial(false);
+                        setClickedTestimonial(null);
+                      }}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      <i className="fa-solid fa-xmark"></i>
+                    </div>
+                    <div className="box-testimonials-image">
+                      <img
+                        src={clickedTestimonial?.testimg_small}
+                        alt="Testimonial"
+                        className="modal-testimonial-image"
+                      />
+                    </div>
+                    <p>
+                      {clickedTestimonial?.testmsg || 'No testimonial text available'}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </section>
+        )
+      }
+    </>
   );
 }
