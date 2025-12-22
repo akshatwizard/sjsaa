27fb76c04c124c7a9b231471dffb39c5bbc5ada2
akshatwizard <akshat@wizards.co.in>
2025-12-22 15:02:01 +0530

    Update StickyBtn.jsx

--- src/components/StickyBtn.jsx
+++ src/components/StickyBtn.jsx
@@ -6,12 +6,14 @@ export default function StickyBtn() {
 
         
         <div className='sticky-button-container'>
+            {/* 
             <Link to={"https://gdsons.co.in/draft/sjs/sites/default/files/2025-12/TicketsList.pdf"} target='_blank'>
                 <img src="/images/DownloadTicketsList.png" alt="Ticket_Button_Website11" />
             </Link>
             <Link to={"https://formbuilder.ccavenue.com/live/icici-bank/st-johns-alumni-trust"} target='_blank'>
                 <img src="/images/Ticket_Button_Website.png" alt="Ticket_Button_Website" />
             </Link>
+             */}
         </div>
         
     )
